feefea02c266fdc9369e1f994b561628118ad89f
Ihoronya <125043436+Ihoronya@users.noreply.github.com>
2023-07-10 10:42:28 +0000

    Deploying to gh-pages from @ Ihoronya/goit-js-hw-09@8c6c179c1dd7750cb8729576f8c1fbe2c7d31925 🚀

---
+++ Bonus.73c11cc2.js
@@ -0,0 +1,2 @@
+document.addEventListener("mousemove",(function(n){Object.assign(document.documentElement,{style:"\n\t\t--move-x: ".concat(-.005*(n.clientX-window.innerWidth/2),"deg;\n\t\t--move-y: ").concat(.01*(n.clientY-window.innerHeight/2),"deg;\n\t\t")})}));
+//# sourceMappingURL=Bonus.73c11cc2.js.map
